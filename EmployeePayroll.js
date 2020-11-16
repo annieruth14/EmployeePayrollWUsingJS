a8ee8b2b61c5a7d2b7016cc22832f5e0d0d08c78
annieruth14 <ruthannie3@gmail.com>
2020-11-16 18:10:04 +0530

    [Annie]Added: code to validate name

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -23,8 +23,10 @@ class EmployeePayrollData {
         return this._name;
     }
     set name(name) {
-        console.log("Setting: "+ name);
-        this._name = name;
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if(nameRegex.test(name))
+            this._name = name;
+        else throw "Name is Incorrect";
     }
 
     get salary() {
@@ -45,8 +47,13 @@ class EmployeePayrollData {
 let employeePayrollData = new EmployeePayrollData(1, "Neha", 5000);
 console.log(employeePayrollData.toString());
 employeePayrollData.id = 0;
-employeePayrollData.name = "Jeffy";
-console.log(employeePayrollData.toString());
+try {
+    employeePayrollData.name = "jeffy";
+    console.log(employeePayrollData.toString()); 
+}
+catch(e) {
+    console.error(e);
+}
 
 let employeePayrollData2 = new EmployeePayrollData(1, "Tressa" , 3000, "F", new Date());
 console.log(employeePayrollData2.toString());
